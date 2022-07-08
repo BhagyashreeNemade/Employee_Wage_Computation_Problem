349e2ca05bdfaca0f023aa1aba2d9b16e207562b
BhagyashreeNemade <108821937+BhagyashreeNemade@users.noreply.github.com>
2022-07-08 21:17:01 +0530

    Employee Attendance

---
+++ Employee_Attendance.sh
@@ -0,0 +1,12 @@
+#! /bin/bash -x
+
+#Check Employee is Present or Absent
+
+echo "Employee Wage Computation"
+attend=$(( $RANDOM%2 ))
+if (( $attend==0 ))
+then
+	echo "Employee is Absent"
+else
+	echo "Employee  is Present"
+fi
